5b9cf5cbb49391a8fe297b9e586c02f3c892218f
TechWithAkash <vishwakarmaakashav17@gmail.com>
2025-10-03 17:11:39 +0530

    Revert "try to solved google login issue"
    This reverts commit 265817aead85738df94d980365599b8c3884e79c.

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,6 @@
 // next.config.mjs
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['mywealthwise.tech', 'www.mywealthwise.tech']
-    }
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/auth/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://mywealthwise.tech' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ]
-      }
-    ]
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
